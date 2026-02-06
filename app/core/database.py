3d331bfae161d2a3f64d801f38200936f4fd2cdc
mangokiwi02 <imchangsu12@naver.com>
2026-02-06 14:01:06 +0900

    이슈 클러스터링 db 삽입

--- app/core/database.py
+++ app/core/database.py
@@ -2,7 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from app.core.config import settings
 
-engine = create_engine(settings.DATABASE_URL)
+# Force pg8000 driver
+db_url = settings.DATABASE_URL.replace("postgresql://", "postgresql+pg8000://")
+
+engine = create_engine(
+    db_url
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
