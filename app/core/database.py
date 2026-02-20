a0dffb5f6aed4477da697a63f8de9297ae155f54
Park-Youngho <2171180@hansung.ac.kr>
2026-02-20 15:22:26 +0900

    db 파일 수정

--- app/core/database.py
+++ app/core/database.py
@@ -1,6 +1,10 @@
 import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+
+# .env 파일 로드
+load_dotenv()
 
 db_url = os.getenv("DATABASE_URL")
 
